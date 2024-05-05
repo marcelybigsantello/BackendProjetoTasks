7dd30294b69d82e6f873bd982e96fd7ba6f91951
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2024-05-05 19:59:24 -0300

    Melhoria endpoint de cadastro task

--- routes/persistence/CRUD_Tasks.mjs
+++ routes/persistence/CRUD_Tasks.mjs
@@ -1,20 +1,10 @@
 import { openDb } from '../services/configDatabase.mjs';
 
 export async function createTasks(newTask){
-    console.log("Creating a new task...", newTask);
-
     let taskData = Object.values(newTask); 
-    console.log("TaskData: ", taskData);
     const db = await openDb();
-    db.run('INSERT INTO Tasks (idTask, description, dateOfConclusion) VALUES '
-    + '($idTask, $description, $dateOfConclusion);', taskData, (error) => {
-        if (error) {
-            console.error("Mensagem de erro: ", error.message);
-            return error;
-        }
-    });
-
-    return newTask;
+    const sql = "INSERT INTO Tasks (idTask, description, dateOfConclusion) VALUES ($idTask, $description, $dateOfConclusion);"
+    return db.run(sql, taskData);
 }
 
 export async function loadTasks() {
